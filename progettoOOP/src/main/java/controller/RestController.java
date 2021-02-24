3ac6d0e43baea7579ece4ad26f9ad7083556ca98
emanueledilore <78969653+emanueledilore@users.noreply.github.com>
2021-02-24 18:49:05 +0100

    rest

--- progettoOOP/src/main/java/controller/RestController.java
+++ progettoOOP/src/main/java/controller/RestController.java
@@ -1,5 +1,15 @@
 package controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class RestController {
+	@RequestMapping(value="/products")
+	public ResponseEntity<Object> prova(){
+		return 1;
+	}
 
 }
