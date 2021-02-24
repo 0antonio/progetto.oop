4bd4131c0d4acdbb9f711f9444e5cb7199d0790e
Giangrossi Antonio <s1084060@studenti.univpm.it>
2021-02-24 18:05:48 +0100

    package controller

--- progettoOOP/src/main/java/univpm/progettoOOP/ProgettoOopApplication.java
+++ progettoOOP/src/main/java/univpm/progettoOOP/ProgettoOopApplication.java
@@ -2,9 +2,19 @@ package univpm.progettoOOP;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+/**
+ * 
+ * @author AntonioGiangrossi
+ * @author EmanueleDiLorenzo
+ *
+ */
 @SpringBootApplication
 public class ProgettoOopApplication {
+/**
+ * 
+ * @param args indica gli argomenti del main
+ *
+ */
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProgettoOopApplication.class, args);
