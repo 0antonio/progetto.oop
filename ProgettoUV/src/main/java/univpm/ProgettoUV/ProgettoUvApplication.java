cdea6f996d56e82ccdecca95307350a982edf7cf
emanueledilore <78969653+emanueledilore@users.noreply.github.com>
2021-02-24 22:59:46 +0100

    comm

--- ProgettoUV/src/main/java/univpm/ProgettoUV/ProgettoUvApplication.java
+++ ProgettoUV/src/main/java/univpm/ProgettoUV/ProgettoUvApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ProgettoUvApplication {
+	int a = 1;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProgettoUvApplication.class, args);
