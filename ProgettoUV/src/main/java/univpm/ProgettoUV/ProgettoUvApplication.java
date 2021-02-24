d1d3569914ce9b522d802b64ca1b5fdef7c80d1e
emanueledilore <78969653+emanueledilore@users.noreply.github.com>
2021-02-24 22:43:02 +0100

    first

---
+++ ProgettoUV/src/main/java/univpm/ProgettoUV/ProgettoUvApplication.java
@@ -0,0 +1,13 @@
+package univpm.ProgettoUV;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgettoUvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgettoUvApplication.class, args);
+	}
+
+}
---
+++ ProgettoUV/src/test/java/univpm/ProgettoUV/ProgettoUvApplicationTests.java
@@ -0,0 +1,13 @@
+package univpm.ProgettoUV;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgettoUvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
