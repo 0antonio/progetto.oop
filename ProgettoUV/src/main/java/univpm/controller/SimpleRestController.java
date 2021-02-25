80664242d0792da7f5429b4db29f4508c8538583
emanueledilore <78969653+emanueledilore@users.noreply.github.com>
2021-02-25 13:25:56 +0100

    prova

---
+++ ProgettoUV/src/main/java/univpm/controller/SimpleRestController.java
@@ -0,0 +1,17 @@
+package univpm.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import univpm.model.HelloWorldClass;
+
+@RestController
+public class SimpleRestController {
+	@GetMapping("/hello")
+	public HelloWorldClass exampleMethod(@RequestParam(name = "param1", defaultValue = "World") String param1) {
+		
+		return new HelloWorldClass("prova", "funziona");
+	}
+
+}
