b4780e43ea5094b2b3d27aebf69eee9a026dded6
Giangrossi Antonio <s1084060@studenti.univpm.it>
2021-02-25 17:40:56 +0100

    Apicall

--- ProgettoUV/src/main/java/univpm/ProgettoUV/controller/MainController.java
+++ ProgettoUV/src/main/java/univpm/ProgettoUV/controller/MainController.java
@@ -1,6 +1,7 @@
 package univpm.ProgettoUV.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,8 +9,8 @@ import univpm.ProgettoUV.model.*;
 
 @RestController
 public class MainController {
-	@GetMapping("/hello")
-	public Prova exampleMethod(@RequestParam(name = "param1", defaultValue = "world") String param1) {
-		return new Prova("funziona", param1);
+	@GetMapping(value="/città/{namecity}",produces = "application/json")
+	public Object exampleMethod(@PathVariable("namecity") String namecity) {
+		return APIcoordinates.Getcoordinates(namecity);
 	}
 }
---
+++ ProgettoUV/src/main/java/univpm/ProgettoUV/model/APIcoordinates.java
@@ -0,0 +1,50 @@
+package univpm.ProgettoUV.model;
+
+import java.io.*;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.JSONValue;
+import org.json.simple.parser.ParseException;
+
+public class APIcoordinates {
+	
+	public static String Getcoordinates(float lat,float lon) {
+		String url = "http://api.openweathermap.org/data/2.5/uvi/forecast?lat="+lat+"&lon="+lon+"&appid=67d40513b0e3e715b6cec6f7e02d354d"; 
+		//api.openweathermap.org/geo/1.0/direct?q="+nome+"&limit=50&appid=67d40513b0e3e715b6cec6f7e02d354d
+		
+		try {
+			
+			URLConnection openConnection = new URL(url).openConnection();
+			InputStream in = openConnection.getInputStream();
+			
+			 String data = "";
+			 String line = "";
+			 try {
+			   InputStreamReader inR = new InputStreamReader( in );
+			   BufferedReader buf = new BufferedReader( inR );
+			  
+			   while ( ( line = buf.readLine() ) != null ) {
+				   data+= line;
+			   }
+			 } finally {
+			   in.close();
+			 }
+			JSONArray obj = (JSONArray) JSONValue.parseWithException(data);	//parse JSON Array
+			
+		} catch (IOException | ParseException e) {
+			e.printStackTrace();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		//conversione
+		
+	 return "ok";
+	}
+	}
+ 
+
+
