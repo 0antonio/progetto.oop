21fde9eb03f50a48773969f57f5f374ce06000c0
Giangrossi Antonio <s1084060@studenti.univpm.it>
2021-02-26 18:10:49 +0100

    carica file.

--- ProgettoUV/src/main/java/univpm/ProgettoUV/controller/MainController.java
+++ ProgettoUV/src/main/java/univpm/ProgettoUV/controller/MainController.java
@@ -5,14 +5,20 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import univpm.ProgettoUV.exception.WrongCoordinatesException;
 import univpm.ProgettoUV.model.*;
 
 @RestController
 public class MainController {
 	@GetMapping(value="/città",produces = "application/json")
 	public Object exampleMethod(
-			@RequestParam("lon") float lon,
-			@RequestParam("lat") float lat) {
-		return APICoordinates.getCoordinates(lon, lat);
+			@RequestParam("name") String name,
+			@RequestParam("country") String country) throws WrongCoordinatesException {
+		
+		double lat = APICoordinates.getCitylat(APICoordinates.caricaArray(),name,country);
+		double lon = APICoordinates.getCitylat(APICoordinates.caricaArray(),name,country);
+		
+		return APICoordinates.getCoordinates(lat,lon);
+		
 	}
 }
--- ProgettoUV/src/main/java/univpm/ProgettoUV/model/APICoordinates.java
+++ ProgettoUV/src/main/java/univpm/ProgettoUV/model/APICoordinates.java
@@ -1,7 +1,6 @@
 package univpm.ProgettoUV.model;
 
 import java.io.*;
-import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
 
@@ -9,10 +8,14 @@ import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.JSONValue;
 import org.json.simple.parser.ParseException;
+import univpm.ProgettoUV.exception.WrongCoordinatesException;
+
+import org.json.simple.parser.JSONParser;
 
 public class APICoordinates {
+	private static String filename = "city.list.json";	
 	
-	public static String getCoordinates(float lat,float lon) {
+	public static String getCoordinates(double lat,double lon) {
 		String url = "http://api.openweathermap.org/data/2.5/uvi/forecast?lat="+lat+"&lon="+lon+"&appid=67d40513b0e3e715b6cec6f7e02d354d"; 
 		//api.openweathermap.org/geo/1.0/direct?q="+nome+"&limit=50&appid=67d40513b0e3e715b6cec6f7e02d354d
 		 String data = "";
@@ -40,11 +43,69 @@ public class APICoordinates {
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		//conversione
 		
 	 return data;
 	}
+	
+	public static JSONArray caricaArray() {
+		JSONParser jsonParser = new JSONParser();
+		JSONArray cityList = null;
+		
+		try (FileReader reader = new FileReader(filename)){
+			//Read JSON file
+			Object obj = jsonParser.parse(reader);
+			cityList = new JSONArray();
+			return cityList = (JSONArray) obj;
+
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+		return cityList;
 	}
+	
+	
+	public static double getCitylat(JSONArray ja,String nameCity,String country) throws WrongCoordinatesException {
+		//Get city object within list
+		double lat=0;
+		for(int i =0; i<ja.size(); i++) {
+			JSONObject cityObject = (JSONObject) ja.get(i);
+			if(cityObject.get("name").equals(nameCity)) {
+				if(cityObject.get("country").equals(country)) {
+					//Get coord object within list
+					JSONObject coordObject = (JSONObject) cityObject.get("coord");
+					//Get lat
+					lat = (Double) coordObject.get("lat");  
+				}
+			}
+		} 
+		if (lat == 0 ) throw new WrongCoordinatesException();
+		return lat;
+	}
+	
+	public static double getCitylon(JSONArray ja,String nameCity,String country) throws WrongCoordinatesException {
+		//Get city object within list
+		double lon=0;
+		for(int i =0; i<ja.size(); i++) {
+			JSONObject cityObject = (JSONObject) ja.get(i);
+			if(cityObject.get("name").equals(nameCity)) {
+				if(cityObject.get("country").equals(country)) {
+					//Get coord object within list
+					JSONObject coordObject = (JSONObject) cityObject.get("coord");
+					//Get lon
+					lon = (Double) coordObject.get("lon");    
+					 
+				}
+			}
+		} 
+		if (lon == 0) throw new WrongCoordinatesException();
+		return lon;
+	}
+	
+}
  
 
 
