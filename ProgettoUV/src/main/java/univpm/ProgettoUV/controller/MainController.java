d6209875c4eb7a7fdc94c84dd270065376ecd562
emanueledilore <78969653+emanueledilore@users.noreply.github.com>
2021-03-02 18:55:04 +0100

    listaCittà

--- ProgettoUV/src/main/java/univpm/ProgettoUV/controller/MainController.java
+++ ProgettoUV/src/main/java/univpm/ProgettoUV/controller/MainController.java
@@ -1,6 +1,13 @@
 package univpm.ProgettoUV.controller;      
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -10,7 +17,7 @@ import univpm.ProgettoUV.model.*;
 
 @RestController
 public class MainController {
-	@GetMapping(value="/città",produces = "application/json")
+	@GetMapping(value="/nomeCittà",produces = "application/json")
 	public String exampleMethod(
 			@RequestParam("name") String name,
 			@RequestParam("country") String country) throws WrongCoordinatesException {
@@ -32,4 +39,58 @@ public class MainController {
 		//return  APICoordinates.getCoordinates(lat,lon).size();
 		return message;
 	}
+	
+	@GetMapping(value="/coorCittà",produces = "application/json")
+	public String restituisciElenco(
+			@RequestParam("lat") String lat,
+			@RequestParam("lon") String lon) throws WrongCoordinatesException  {
+				String message = "";
+				String[] listLat = lat.split(",");
+				String[] listLon = lon.split(",");
+				int i=0;
+				for(String latElement:listLat) {
+					String lonElement = listLon[i];
+						i=i++;
+						
+						// conversione a double
+						double latitudine = Double.parseDouble(latElement);
+						double longitudine = Double.parseDouble(lonElement);
+										
+				message+="\n"+latitudine +"  "+longitudine+"\n"; // ci devo mettere nome città e country
+				message+=APICoordinates.getCoordinates(latitudine,longitudine);
+				
+				}
+				//return  APICoordinates.getCoordinates(lat,lon).size();
+				return message;
+				
+	}
+	
+	@GetMapping(value="/listaCittà",produces = "application/json")
+	public JSONArray listaCit() throws FileNotFoundException {
+		JSONArray obj= null;
+		
+		try {
+		JSONParser jsonParser = new JSONParser();
+		JSONArray cityList = null;
+         FileReader reader = new FileReader("city.list.json");
+			//Read JSON file
+			
+			
+				obj = (JSONArray) jsonParser.parse(reader);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (ParseException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		
+		
+		
+		
+		return obj;
+	}
+	
+	
+	
 }
