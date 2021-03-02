2079ff219d4403be4a9ca6af68f4451593b6015e
Giangrossi Antonio <s1084060@studenti.univpm.it>
2021-03-02 10:59:43 +0100

    city

--- ProgettoUV/src/main/java/univpm/ProgettoUV/controller/MainController.java
+++ ProgettoUV/src/main/java/univpm/ProgettoUV/controller/MainController.java
@@ -1,49 +1,35 @@
-package univpm.ProgettoUV.controller;
-
-import java.util.ArrayList;
+package univpm.ProgettoUV.controller;      
 
 import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
 import univpm.ProgettoUV.exception.WrongCoordinatesException;
 import univpm.ProgettoUV.model.*;
-import univpm.ProgettoUV.service.*;
 
 @RestController
 public class MainController {
-	@Autowired
-	CittàServiceImpl città;
-	
-	@GetMapping(value="/risultato",produces = "application/json")
-	public Object restituisciValori() throws WrongCoordinatesException {
-		JSONObject out = new JSONObject();
-		String identifier = "";
-		ArrayList<Città> cit = città.getCittà();
-		for (int i = 0; i<cit.size();i++) {
-			Città tmp = cit.get(i);
-			identifier = tmp.getName() +", " +tmp.getCountry();
-		out.put(identifier, APICoordinates.getValues(tmp.getName(),tmp.getCountry()));
-		}
-		return out;
-	}
-	
-	@PostMapping(value="/città",produces = "application/json")
-	public ResponseEntity<Object> inserisciCittà(
+	@GetMapping(value="/città",produces = "application/json")
+	public String exampleMethod(
 			@RequestParam("name") String name,
-			@RequestParam("country") String country)  throws WrongCoordinatesException{ //cambiare exception 
+			@RequestParam("country") String country) throws WrongCoordinatesException {
+		String message = "";
+		String[] list = name.split(",");
+		String[] countrylist = country.split(",");
 		
-		città.inserisciCittà(name, country);
+		int i=0;
+		for(String element:list) {
+			String countryelement = countrylist[i];
+				i=i++;
+		double lat = APICoordinates.getCitylat(APICoordinates.caricaArray(),element,countryelement);
+		double lon = APICoordinates.getCitylat(APICoordinates.caricaArray(),element,countryelement);
 		
+		message+="\n"+element+"  "+countryelement+"\n";
+		message+=APICoordinates.getCoordinates(lat,lon);
+		
+		}
 		//return  APICoordinates.getCoordinates(lat,lon).size();
-		return new ResponseEntity<>("città inserita correttamente", HttpStatus.CREATED);
+		return message;
 	}
-	
 }
